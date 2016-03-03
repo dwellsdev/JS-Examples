ecedb7986165c3c17d5b933ca19aca7c581caa25
Derrick <dwellsdev@gmail.com>
2016-03-04 00:18:18 +0900

    Create 003 PalindromeCheck.js.

---
+++ 003 PalindromeCheck.js
@@ -0,0 +1,22 @@
+// Exercise from FreeCodeCamp.com
+function palindrome(str) {
+  str = str.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+  str = str.toLowerCase()
+  var palCheck = str.split("").reverse().join("");
+  if(str !== palCheck) {
+    return false;
+  }
+  return true;
+}
+
+
+
+palindrome("eye");
+palindrome("not a palindrome");
+palindrome("A man, a plan, a canal. Panama");
+palindrome("never odd or even");
+palindrome("nope");
+palindrome("almostomla");
+palindrome("My age is 0, 0 si ega ym.");
+palindrome("1 eye for of 1 eye.");
+palindrome("0_0 (: /-\ :) 0-0");
